77728c3e5a26cf6d8380c65a3f8a7e7e922be548
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-04-26 14:31:54 +0800

    Added LoadType api controller

---
+++ Source/AEM/SPS.AEM.Repository/Implementations/LoadTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SPS.AEM.Database.Contexts;
+using SPS.AEM.Repository.Interfaces;
+using SPS.AEM.Repository.Models;
+
+namespace SPS.AEM.Repository.Implementations
+{
+    public class LoadTypeRepository : ILoadTypeRepository
+    {
+        private readonly AemDatabaseContext context;
+
+        public LoadTypeRepository(AemDatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<LoadTypeDto>> GetAllLoadTypesAsync()
+        {
+            return await this.context.LoadTypes
+                .Select(p => new LoadTypeDto { Id = p.Id, Name = p.Type })
+                .ToListAsync();
+        }
+    }
+}
--- Source/AEM/SPS.AEM.Web/Startup.cs
+++ Source/AEM/SPS.AEM.Web/Startup.cs
@@ -32,6 +32,7 @@ namespace SPS.AEM.Web
             services.AddDbContext<AemDatabaseContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Aem"], b => b.MigrationsAssembly("SPS.AEM.Web")));
             services.AddControllers();
 
+            services.AddTransient<ILoadTypeRepository, LoadTypeRepository>();
             services.AddTransient<IDistrictRepository, DistrictRepository>();
             services.AddTransient<ITalukaRepository, TalukaRepository>();
             services.AddTransient<IVillageRepository, VillageRepository>();
