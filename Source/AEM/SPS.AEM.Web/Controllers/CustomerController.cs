a9b01f6a73d64038df63627b7c7d558eec0042ce
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-04-26 16:27:17 +0800

    added customer and user controller apis

---
+++ Source/AEM/SPS.AEM.Web/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SPS.AEM.Repository.Interfaces;
+using SPS.AEM.Repository.Models;
+
+namespace SPS.AEM.Web.Controllers
+{
+    [ApiController]
+    [Route("contextapi/customer")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return this.Ok(await customerRepository.GetAllCustomersAsync());
+        }
+
+        [HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("{customerId}")]
+        public async Task<IActionResult> Get(int customerId)
+        {
+            return this.Ok(await customerRepository.GetCustomerByIdAsync(customerId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CustomerDto customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await customerRepository.AddCustomerAsync(customer);
+            return Created($"contextapi/customer/{customer.Id}", customer);
+        }
+    }
+}
---
+++ Source/AEM/SPS.AEM.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SPS.AEM.Repository.Interfaces;
+using SPS.AEM.Repository.Models;
+
+namespace SPS.AEM.Web.Controllers
+{
+    [ApiController]
+    [Route("contextapi/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserDto user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await userRepository.AddUserAsync(user);
+            return Created($"contextapi/user/{user.Id}", user);
+        }
+    }
+}
