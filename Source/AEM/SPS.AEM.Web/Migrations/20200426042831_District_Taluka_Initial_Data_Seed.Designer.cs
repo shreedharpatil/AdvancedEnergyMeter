c209fc1ae2861ba9ef2ced4ef9b9801ec08d29f4
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-04-26 12:31:34 +0800

    Seeded initial values for district and taluka tables

--- Source/AEM/SPS.AEM.Web/Migrations/20200426042831_District_Taluka_Initial_Data_Seed.Designer.cs
+++ Source/AEM/SPS.AEM.Web/Migrations/20200426042831_District_Taluka_Initial_Data_Seed.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SPS.AEM.Database.Contexts;
 
 namespace SPS.AEM.Web.Migrations
 {
     [DbContext(typeof(AemDatabaseContext))]
-    partial class AemDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200426042831_District_Taluka_Initial_Data_Seed")]
+    partial class District_Taluka_Initial_Data_Seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
