f6689ec1e479387867c8c16bf906fa1c5ff1f324
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-04-26 14:40:59 +0800

    Data seed to ROle table

--- Source/AEM/SPS.AEM.Web/Migrations/20200426063901_Role_Table_Data_Seed.Designer.cs
+++ Source/AEM/SPS.AEM.Web/Migrations/20200426063901_Role_Table_Data_Seed.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SPS.AEM.Database.Contexts;
 
 namespace SPS.AEM.Web.Migrations
 {
     [DbContext(typeof(AemDatabaseContext))]
-    partial class AemDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200426063901_Role_Table_Data_Seed")]
+    partial class Role_Table_Data_Seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
