a39d4048248e5e746581b3a8830409a3e0ec38ff
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-05-16 13:14:42 +0800

    Sidebar menu toggle

--- Source/UI/src/app/aem/portfolio/header/header.component.ts
+++ Source/UI/src/app/aem/portfolio/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../login/authentication/auth.service';
 import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -18,5 +19,23 @@ export class HeaderComponent implements OnInit {
   logout() {
     this.authService.logout();
     this.router.navigate(['login']);
+  }
+
+  toggle() {
+    $('.sidebar-toggle').toggleClass('active');
+
+    $('#sidebar').toggleClass('shrinked');
+    $('.page-content').toggleClass('active');
+    $(document).trigger('sidebarChanged');
+
+    if ($('.sidebar-toggle').hasClass('active')) {
+            $('.navbar-brand .brand-sm').addClass('visible');
+            $('.navbar-brand .brand-big').removeClass('visible');
+            $('.sidebar-toggle').find('i').attr('class', 'fa fa-long-arrow-right');
+        } else {
+            $('.navbar-brand .brand-sm').removeClass('visible');
+            $('.navbar-brand .brand-big').addClass('visible');
+            $('.sidebar-toggle').find('i').attr('class', 'fa fa-long-arrow-left');
+        }
   }
 }
