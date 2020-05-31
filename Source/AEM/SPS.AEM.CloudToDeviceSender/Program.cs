e6ddeb6b1753494a0d73f3bc7e222c90805beb39
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-05-31 15:45:18 +0800

    Created Projects to read from Iot hub and api to process the event data

---
+++ Source/AEM/SPS.AEM.CloudToDeviceSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPS.AEM.Shared;
+
+namespace SPS.AEM.CloudToDeviceSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> commands = new Dictionary<string, string>
+            {
+                { "R", EventType.Reading.ToString() },
+                { "B", EventType.BlockDevice.ToString() },
+                { "U", EventType.UnblockDevice.ToString()}
+            };
+            string connectionString = "HostName=SPS-AEM-DEV-EVENTHUB.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=X0elD7P0XElDZrw827wgaLSeSW1ej/gy4khjrHVrpSc=";
+            string deviceId = "MyFirstIoTHubDevice";
+            var serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            try
+            {
+                do
+                {
+                    var commandMessage = new Message();
+                    Console.WriteLine("R -> Reading, B-> Block device, U -> UnblockDevice");
+                    var input = Console.ReadLine();
+                    commandMessage.Properties.Add(new KeyValuePair<string, string>("Command", commands[input]));
+                    commandMessage.Ack = DeliveryAcknowledgement.Full;
+                    serviceClient.SendAsync(deviceId, commandMessage).Wait();
+                    Console.WriteLine("1 -> Send another command, 2 -> stop");
+                } while (Console.ReadLine() == "1");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            
+            Console.WriteLine("Press any key to exit");
+            serviceClient?.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Source/AEM/SPS.AEM.MessageReader/SimpleEventProcessor.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPS.AEM.MessageReader
+{
+    public class SimpleEventProcessor : IEventProcessor, IDisposable
+    {
+        private readonly string ApiUrl;
+        private readonly HttpClient httpClient;
+
+        public SimpleEventProcessor()
+        {
+            ApiUrl = ConfigurationManager.AppSettings["ApiBaseUrl"] + "eventData";
+            httpClient = new HttpClient();
+        }
+
+        Stopwatch checkpointStopWatch;
+
+        async Task IEventProcessor.CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine("Processor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason);
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        Task IEventProcessor.OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine("SimpleEventProcessor initialized.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+            this.checkpointStopWatch = new Stopwatch();
+            this.checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
+        }
+
+        async Task IEventProcessor.ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (EventData eventData in messages)
+            {
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+
+                Console.WriteLine(string.Format("Message received.  Partition: '{0}', Data: '{1}'",
+                    context.Lease.PartitionId, data));
+                var body = new StringContent(data, Encoding.UTF8, "application/json");
+                try
+                {
+                    var r = await httpClient.PostAsync(ApiUrl, body);
+                }
+                catch (Exception e)
+                {
+                }
+            }
+
+            //Call checkpoint every 5 minutes, so that worker can resume processing from 5 minutes back if it restarts.
+            if (this.checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(5))
+            {
+                await context.CheckpointAsync();
+                this.checkpointStopWatch.Restart();
+            }
+        }
+
+        public void Dispose()
+        {
+            httpClient?.Dispose();
+        }
+    }
+}
