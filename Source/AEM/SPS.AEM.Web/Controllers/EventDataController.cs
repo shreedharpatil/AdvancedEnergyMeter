6e63cfdbd83ea2d506e563cc9b7ce8186c175142
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-05-31 18:44:48 +0800

    Implemented signalR to publish received events from IoT hub

--- Source/AEM/SPS.AEM.Web/Controllers/EventDataController.cs
+++ Source/AEM/SPS.AEM.Web/Controllers/EventDataController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using SPS.AEM.Shared;
+using SPS.AEM.Web.Hub;
 
 namespace SPS.AEM.Web.Controllers
 {
@@ -11,15 +13,22 @@ namespace SPS.AEM.Web.Controllers
     [Route("eventData")]
     public class EventDataController : Controller
     {
+        private IHubContext<AemHub> hub;
+
+        public EventDataController(IHubContext<AemHub> hub)
+        {
+            this.hub = hub;
+        }
+
         [HttpPost]
-        public IActionResult Post([FromBody] EventData eventData)
+        public async Task<IActionResult> Post([FromBody] EventData eventData)
         {
             if (eventData == null)
             {
                 return Ok();
             }
 
-
+            await hub.Clients.All.SendAsync("eventReceived", eventData);
             return Ok();
         }
     }
