1320a5a00da10c8b15fa89daae52f1d3dd912415
Shreedhar Patil <Shreedhar.Patil@zuhlke.com>
2020-11-22 22:50:49 +0800

    Enbled lazy loading of routes for data data.configuration and portfolio

--- Source/UI/src/app/aem/portfolio/shared/shared.module.ts
+++ Source/UI/src/app/aem/portfolio/shared/shared.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AuthService } from '../../login/authentication/auth.service';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { EffectsModule } from '@ngrx/effects';
-import { PortfolioEffects } from '../portfolio/portfolio.effects';
-import { SharedDataEffects } from '../../shared/shared.data.effects';
-import { HttpHelperEffects } from '../../shared/http/http-helper-effects';
-import { SpinnerEffects } from '../../shared/spinner/spinner-effects';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
@@ -20,9 +14,7 @@ import { RouterModule } from '@angular/router';
     ],
     imports: [
         RouterModule,
-        BrowserModule,
         HttpClientModule,
-        EffectsModule.forRoot([PortfolioEffects, SharedDataEffects, HttpHelperEffects, SpinnerEffects]),
       ],
     providers: [AuthService],
     exports: [HeaderComponent,
